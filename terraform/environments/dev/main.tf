aa3b992714d0f1e5d7684b9c33516e9e8892bb3b
Flamur Damoni <fdamoni@onna.com>
2026-05-19 00:33:23 +0200

    chore: update owner tag to platform-team-core

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,10 +1,10 @@
 module "s3" {
   source = "../../modules/s3"
 
-  bucket_name        = "my-company-dev-data-890381434210"
-  env                = "dev"
-  project            = "my-company-app"
-  owner              = "platform-team"
+  bucket_name = "my-company-dev-data-890381434210"
+  env         = "dev"
+  project     = "my-company-app"
+  owner       = "platform-team-core"
 
   versioning_enabled = true
 }
